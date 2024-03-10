121aa7a2f1453e4a0b5e3597b8072b2ccdd5dcaf
Marcos Santos <89700349+marcos-nsantos@users.noreply.github.com>
2024-03-10 09:15:20 -0500

    feat: add Installment struct and implement auto migration
    A new file, internal/structs/installment.go, has been created to define the Installment struct. Additionally, auto migration for Installment struct has been implemented within the gorm open function in cmd/api/main.go.

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -9,6 +9,8 @@ import (
 	"github.com/gofiber/fiber/v2"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+
+	"github.com/marcos-nsantos/go-container-pattern/internal/structs"
 )
 
 func main() {
@@ -22,10 +24,11 @@ func main() {
 		os.Getenv("DB_PORT"),
 	))
 
-	_, err := gorm.Open(dialector)
+	db, err := gorm.Open(dialector)
 	if err != nil {
 		log.Fatalf("banco não conectou. Err=%v", err.Error())
 	}
+	db.AutoMigrate(&structs.Installment{})
 
 	server.Get("/heath", func(c *fiber.Ctx) error {
 		return c.Status(http.StatusOK).JSON("pong :)")
